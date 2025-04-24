c2f73b4085560b4b882e8f2891bcd08ec5004e72
1van0V28 <Ivanov.IS1@yandex.ru>
2025-04-25 02:35:40 +1000

    Added StatsSection

--- src/stats_section/StatsSection.jsx
+++ src/stats_section/StatsSection.jsx
@@ -1,11 +1,53 @@
+import { useState, useRef, useEffect } from "react"
 import { Section } from "../section/Section"
 import { Counter } from "./stats_section__counter/Counter"
+import "./stats_section.css"
+
+
+const COUNTERS_LIST = [
+    "пользователей",
+    "тренировок",
+    "скачиваний",
+    "рейтинг"
+]
 
 
 export function StatsSection() {
+    const [statsState, setStatsState] = useState(false)
+    
+    const countersRef = useRef([])
+
+    const countersList = COUNTERS_LIST.map((counter, index) => {
+        return (
+            <Counter key={index + 1} 
+            name={counter} 
+            isVisible={statsState}
+            ref={(el) => {countersRef.current[index] = el}}/>
+        )
+    })
+
+    useEffect(() => {
+        const countersObserver = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting && !statsState) {
+                    setStatsState(true)
+                    countersObserver.unobserve(entry.target)
+                } else if (statsState) {
+                    countersObserver.unobserve(entry.target)
+                }
+            })
+        }, {threshold: 1})
+
+        countersRef.current.forEach((counter) => {
+            countersObserver.observe(counter)
+        })
+    }, [])
+
     return (
-        <Section header={"Пик наших достижений"}>
-            <Counter /> {/*список в ряд, для мобилок - в колонку*/}
+        <Section title={"Пик наших достижений"} background={"to_light"}>
+            <div className="stats_section_counters_container">
+                {countersList}
+            </div>
         </Section>
     )
 }
--- src/stats_section/stats_section__counter/Counter.jsx
+++ src/stats_section/stats_section__counter/Counter.jsx
@@ -1,8 +1,67 @@
-export function Counter() {
+import { useState, useEffect } from "react"
+import "./stats_section__counter.css"
+
+
+const COUNTERS_LIST = {
+    "пользователей": {
+        maxCount: 500,
+        text: "K+"
+    },
+    "тренировок": {
+        maxCount: 10,
+        text: "M+"
+    },
+    "скачиваний": {
+        maxCount: 1000,
+        text: "K+"
+    },
+    "рейтинг": {
+        maxCount: 4.8,
+        text: "/5"
+    }
+}
+
+
+const getCounterParams = function(maxCount) {
+    maxCount = String(maxCount)
+    if (maxCount.includes(".")) {
+        const pointIndex = maxCount.indexOf(".")
+        const numberOfDigits = maxCount.slice(pointIndex + 1).length
+        return {
+            increment: 1 / (Math.pow(10, numberOfDigits)),
+            incrementTime: ((3 / Number(maxCount)) * 1000) / Math.pow(10, numberOfDigits)
+        }
+    }
+
+    return {
+        increment: 1,
+        incrementTime: (3 / Number(maxCount)) * 1000,
+    }
+}
+
+
+export function Counter({name, isVisible, ref}) {
+    const [counterState, setCounterState] = useState(0)
+
+    useEffect(() => {
+        let startCount = 0
+        const maxCount = COUNTERS_LIST[name].maxCount
+        const counterParams = getCounterParams(maxCount)
+
+        const timer = setInterval(() => {
+            const newStartCount = String(startCount + counterParams.increment).slice(0, String(maxCount).length)
+            startCount += counterParams.increment
+            setCounterState(Number(newStartCount))
+            if (startCount >= maxCount) {
+                clearInterval(timer)
+            }
+        }, counterParams.incrementTime)
+    }, [isVisible])
+
     return (
-        <div>
-            <p></p>
-            <p></p>
+        <div className="stats_section__counter" ref={ref}>
+            <p className="stats_section__number">{`${counterState + COUNTERS_LIST[name].text}`}</p>
+            <p className="stats_section__text">{name}</p>
         </div>
     )
 }
