553e4d7f3736681ef068b9af2afc9db0a16dc4e7
1van0V28 <Ivanov.IS1@yandex.ru>
2025-04-25 21:13:24 +1000

    Added NewsletterSection

--- src/newsletter_section/NewsletterSection.jsx
+++ src/newsletter_section/NewsletterSection.jsx
@@ -1,10 +1,13 @@
 import { Section } from "../section/Section"
+import { NewsletterForm } from "./newsletter_section__newsletter_form/NewsletterForm"
+import "./newsletter_section.css"
 
 
 export function NewsletterSection() {
     return (
-        <Section header={"Получи бесплатный план тренировок"}>
-
+        <Section title={"Подпишись на новости"} background={"to_dark"}>
+            <h2 className="newsletter_section__subtitle">Получай советы по тренировкам и новости PeakPulse прямо на почту!</h2>
+            <NewsletterForm/>
         </Section>
     )
 }
--- src/newsletter_section/newsletter_section__newsletter_form/NewsletterForm.jsx
+++ src/newsletter_section/newsletter_section__newsletter_form/NewsletterForm.jsx
@@ -1,10 +1,76 @@
+import { useState } from "react"
+import "./newsletter_section__newsletter_form.css"
+
+
+const getInputStyle = function(name, state) {
+    let inputStyle = "newsletter_section__input"
+    if (state[name]) {
+        inputStyle += " newsletter_section__input--filled"
+    }
+
+    return inputStyle
+}
+
+
+const isFulfilled = function(state) {
+    for (let value of Object.values(state)) {
+        if (!value) {
+            return false
+        }
+    }
+
+    return true
+}
+
+
 export function NewsletterForm() {
+    const [formState, setFormState] = useState({
+        email: "",
+        name: "",
+        acception: false,
+    })
+
+    const handleInputChange = function(event) {
+        const {name, value, type, checked} = event.target
+        if (type === "email" || type === "text") {
+            setFormState({...formState, [name]: value})
+        } else if (type === "checkbox") {
+            setFormState({...formState, [name]: checked})
+        }
+    }
+
     return (
-        <form>
-            <input type="email"></input>
-            <input type="text"></input>
-            <input type="checkbox"></input>
-            <input type="submit"></input>
+        <form className="newsletter_section__newsletter_form">
+            <input className={getInputStyle("email", formState)} 
+            type="email"
+            name="email"
+            placeholder="Твой email" 
+            value={formState.email}
+            onChange={handleInputChange}>
+            </input>
+
+            <input className={getInputStyle("name", formState)} 
+            type="text" 
+            name="name"
+            placeholder="John Doe" 
+            value={formState.name}
+            onChange={handleInputChange}>
+            </input>
+
+            <label className="newsletter_section__acception">
+                <input className="newsletter_section__checkbox" 
+                type="checkbox" 
+                name="acception"
+                onChange={handleInputChange}>
+                </input>
+                Я согласен(на) получать информационную рассылку
+            </label>
+
+            {isFulfilled(formState) ?
+            <input className="newsletter_section__submit newsletter_section__submit--active" type="submit" value="Подписаться"></input>
+            :
+            <input className="newsletter_section__submit" type="submit" value="Подписаться" disabled></input>
+            }
         </form>
     )
 }
